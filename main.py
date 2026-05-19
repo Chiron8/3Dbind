602a5ad049b1a51bcc7cdfd5e9d65edd4dc06d18
Chiron <135429057+Chiron8@users.noreply.github.com>
2026-05-19 18:28:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import board # Allows the MC to communicate with the computer
 import adafruit_mmc56x3 # Allows MC to communicate with magetometer
 import neopixel # Allows MC to use onboard LED
 import usb_hid # These 4 packages allows MC to simulate keyboard and mouse
+import digitalio # Allows the MC to detect external buttons
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 from adafruit_hid.mouse import Mouse
@@ -17,6 +18,11 @@ m = Mouse(usb_hid.devices) # Shortcut code to refer to mouse
 i2c = board.STEMMA_I2C()  # Shortcut code to refer to how magnetometer is connected to MC
 sensor = adafruit_mmc56x3.MMC5603(i2c) # Shortcut code to refer to magnetometer
 
+# Button setup on A0
+button = digitalio.DigitalInOut(board.A0)
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.UP
+
 threshold = 100 # How far the handle needs to move before MC starts keybind action
 
 # Creates 3 average axis variables
@@ -24,22 +30,18 @@ X = 0
 Y = 0
 Z = 0
 
-avrgList = [] # Creates 'list of average readings'
 
-# Adds magnetometer readings to 'list of average readings'
-for i in range(1, 4):
-    pixel.fill((0, 0, 255))
-    mag_x, mag_y, mag_z = sensor.magnetic
-    avrgList.append(mag_x)
-    avrgList.append(mag_y)
-    avrgList.append(mag_z)
-    time.sleep(0.5)
+# Sum 3 readings for each axis then divide by 3 to get mean average
+for i in range(3):
+   mag_x, mag_y, mag_z = sensor.magnetic
+   X += mag_x
+   Y += mag_y
+   Z += mag_z
+   time.sleep(0.5)
+X /= 3
+Y /= 3
+Z /= 3
 
-# Takes data from 'list of average readings' and creates an average for each axis
-# This is how the program reads zero when the magnet/handle is stationary
-X = (avrgList[0] + avrgList[3] + avrgList[6]) / 3
-Y = (avrgList[1] + avrgList[4] + avrgList[7]) / 3
-Z = (avrgList[2] + avrgList[5] + avrgList[8]) / 3
 
 pixel.fill((255, 40, 80)) # Sets LED colour to pink
 
@@ -59,6 +61,12 @@ while True:
             for i in range(7): # Keybind 'scroll down' mouse wheel to zoom in
                 m.move(0, 0, -1)
                 time.sleep(0.05)
+
+    # if button is pressed simulate pressing S key, if it isn't release the S key
+    if not button.value:
+        kbd.press(Keycode.S)
+    else:
+        kbd.release(Keycode.S)
 
     # BELOW IS A GENERAL EXPLAINATION ON HOW THE PROGRAM MANAGES KEYBINDS IN RELATION TO THE HANDLE MOVEMENT:
     # if mag_s - S < -threshold...:            CHECKS IF HANDLE HAS BEEN MOVED
